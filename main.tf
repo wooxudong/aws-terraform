3482a8bfb48c56a5cf4dc44efa0d40e0346297fa
Wu Xudong <wooxudong@gmail.com>
2021-03-09 13:25:20 +0800

    added setup for simple http server

--- main.tf
+++ main.tf
@@ -9,49 +9,27 @@ resource "aws_default_vpc" "default" {
   }
 }
 
-resource "aws_security_group" "sec-group-1" {
-  name = "allow ICMP - 1"
+resource "aws_security_group" "web-access" {
+  name = "allow http access"
   description = "this will allow ICMP from instance 2 to instance 1"
   vpc_id = aws_default_vpc.default.id
 
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 }
 
-resource "aws_security_group_rule" "sec-group-rule-1" {
-  type = "ingress"
-  from_port = 8
-  to_port = 0
-  protocol = "icmp"
-  security_group_id = aws_security_group.sec-group-1.id
-  source_security_group_id = aws_security_group.sec-group-2.id
-}
-
-resource "aws_security_group" "sec-group-2" {
-  name = "allow ICMP - 2"
-  description = "this will allow ICMP from instance 1 to instance 2"
-  vpc_id = aws_default_vpc.default.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group_rule" "sec-group-rule-2" {
-  type = "ingress"
-  from_port = 8
-  to_port = 0
-  protocol = "icmp"
-  security_group_id = aws_security_group.sec-group-2.id
-  source_security_group_id = aws_security_group.sec-group-1.id
-}
 
 resource "aws_security_group" "allow-ssh" {
   name = "allow ssh"
@@ -66,26 +44,27 @@ resource "aws_security_group" "allow-ssh" {
   }
 }
 
-resource "aws_instance" "ins-1" {
+resource "aws_instance" "ins" {
   ami = "ami-0d06583a13678c938"
   instance_type = "t2.micro"
   security_groups = [
-    aws_security_group.sec-group-1.name,
+    aws_security_group.web-access.name,
     aws_security_group.allow-ssh.name]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install httpd -y
+              systemctl start httpd
+              systemctl enable httpd
+              cd /var/www/html
+              echo "This is a test page for fun" >> index.html
+              EOF
   tags = {
-    Name = "Instance 1"
+    Name = "Simple Web Server"
   }
 }
 
-resource "aws_instance" "ins-2" {
-  ami = "ami-0d06583a13678c938"
-  instance_type = "t2.micro"
-  security_groups = [
-    aws_security_group.sec-group-2.name,
-    aws_security_group.allow-ssh.name]
-
-  tags = {
-    Name = "Instance 2"
-  }
+output "instance_public_ip" {
+  value = aws_instance.ins.public_ip
 }
